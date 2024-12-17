384501307dfb07541ab44f00a54fc0a3a226a86a
Ayush Patil <94798136+ayushppatil@users.noreply.github.com>
2024-12-17 11:13:03 +0530

    Initial commit

---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+// void main ()
+// {
+//     int i = 1, j;
+
+
+//     while (i<=3)
+
+//     for ( j = 1; j <=  i; i++)
+//     {
+//         printf("* ");
+//     }
+    
+//     printf("\n");
+    
+//     i++;
+
+    
+// }
